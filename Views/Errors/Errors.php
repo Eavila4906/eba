21a1cedd62f7dd6e91a1fb555ca0023241b412a7
eavila4906-utm <eavila4906@utm.edu.ec>
2021-09-10 14:07:38 -0500

    Mejora en la vista de pagina no encontrada

--- Views/Errors/Errors.php
+++ Views/Errors/Errors.php
@@ -4,9 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina no encontrada</title>
+    <title>Page not found - <?=NAME_PROJECT;?></title>
+    <link rel="icon" href="<?= MEDIA(); ?>images/icons/icon.ico">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 </head>
 <body>
-    <H1>Pagina no encontrada</H1>
+    <style>
+        body {
+            background: #dedede;
+        }
+        .page-wrap {
+            min-height: 100vh;
+        }
+    </style>
+    <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block text-secondary">404</span>
+                <h1 class="text-secondary">OOPS! PAGE NOT BE FOUND</h1>
+                <div class="mb-4 lead text-secondary">
+                    Sorry, but the page you are looking for does not exist, it has been deleted, the name has been changed<br> 
+                    or is temporarily unavailable  
+                </div>
+                <a href="<?=BASE_URL();?>" class="btn btn-link">Back to homepage</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </html>
