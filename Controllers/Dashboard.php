3daa3f1723f179767fc087412beb05f22aa7e33f
eavila4906-utm <eavila4906@utm.edu.ec>
2021-09-10 20:43:54 -0500

    Validación de permisos a los metodos del MD

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -7,42 +7,85 @@
         }
 
         public function Dashboard(){
+            if (empty($_SESSION['permisosModulo']['r'])) {
+                if ($_SESSION['dataUser']['nombreRol'] == 'Super Administrador') {
+                    header('location: '.BASE_URL().'dashboard');
+                } else {
+                    header('location: '.BASE_URL().'my');
+                }
+            }
             $data['functions_js'] = "./Assets/js/functions_dashboard.js";
             $this->views->getViews($this,"dashboard", $data);
         }
 
         public function getCountUsers() {
-            $arrData = $this->model->selectCountUsers();
-            if (!empty($arrData)) {
-                $arrayData = array('status' => true, 'data' => $arrData);
-                echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+            if ($_SESSION['permisosModulo']['r']) {
+                $arrData = $this->model->selectCountUsers();
+                if (!empty($arrData)) {
+                    $arrayData = array('status' => true, 'data' => $arrData);
+                    echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert" 
+                        style="position: relative;padding: 0.75rem 1.25rem;margin-bottom: 1rem;border: 
+                        1px solid transparent;border-radius: 0.25rem;color: #721c24;background-color: #f8d7da;
+                        border-color: #f5c6cb;border-top-color: #f1b0b7;">
+                        <b>¡Restricted access!</b>: you do not have permission to manipulate this module.
+                      </div>';
             }
             die();
         }
 
         public function getCountCourses() {
-            $arrData = $this->model->selectCountCourses();
-            if (!empty($arrData)) {
-                $arrayData = array('status' => true, 'data' => $arrData);
-                echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+            if ($_SESSION['permisosModulo']['r']) {
+                $arrData = $this->model->selectCountCourses();
+                if (!empty($arrData)) {
+                    $arrayData = array('status' => true, 'data' => $arrData);
+                    echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert" 
+                        style="position: relative;padding: 0.75rem 1.25rem;margin-bottom: 1rem;border: 
+                        1px solid transparent;border-radius: 0.25rem;color: #721c24;background-color: #f8d7da;
+                        border-color: #f5c6cb;border-top-color: #f1b0b7;">
+                        <b>¡Restricted access!</b>: you do not have permission to manipulate this module.
+                    </div>';
             }
             die();
         }
 
         public function getCountStudens() {
-            $arrData = $this->model->selectCountStudens();
-            if (!empty($arrData)) {
-                $arrayData = array('status' => true, 'data' => $arrData);
-                echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+            if ($_SESSION['permisosModulo']['r']) {
+                $arrData = $this->model->selectCountStudens();
+                if (!empty($arrData)) {
+                    $arrayData = array('status' => true, 'data' => $arrData);
+                    echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert" 
+                        style="position: relative;padding: 0.75rem 1.25rem;margin-bottom: 1rem;border: 
+                        1px solid transparent;border-radius: 0.25rem;color: #721c24;background-color: #f8d7da;
+                        border-color: #f5c6cb;border-top-color: #f1b0b7;">
+                        <b>¡Restricted access!</b>: you do not have permission to manipulate this module.
+                    </div>';
             }
             die();
         }
 
         public function getCountTeachers() {
-            $arrData = $this->model->selectCountTeachers();
-            if (!empty($arrData)) {
-                $arrayData = array('status' => true, 'data' => $arrData);
-                echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+            if ($_SESSION['permisosModulo']['r']) {
+                $arrData = $this->model->selectCountTeachers();
+                if (!empty($arrData)) {
+                    $arrayData = array('status' => true, 'data' => $arrData);
+                    echo json_encode($arrayData, JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert" 
+                        style="position: relative;padding: 0.75rem 1.25rem;margin-bottom: 1rem;border: 
+                        1px solid transparent;border-radius: 0.25rem;color: #721c24;background-color: #f8d7da;
+                        border-color: #f5c6cb;border-top-color: #f1b0b7;">
+                        <b>¡Restricted access!</b>: you do not have permission to manipulate this module.
+                    </div>';
             }
             die();
         }
