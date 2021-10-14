5753e3442d55023ea4f3c7bf873b509e240cd95c
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-14 12:41:45 -0500

    Improvements in the payment reporting module (model)

--- Models/PaymentModel.php
+++ Models/PaymentModel.php
@@ -6,18 +6,21 @@
 
         public function SelectAllPayment() {
             $Query_Select_All = "SELECT  pa.id_payment, estudiante AS DNI, CONCAT(us.nombres, ' ', us.apellidoP, ' ', us.apellidoM) AS estudiante
-                                    FROM payment pa INNER JOIN usuario us ON (pa.estudiante=us.DNI) GROUP BY estudiante";
+                                    FROM detail_payment dp INNER JOIN payment pa ON (dp.id_detail_payment=pa.id_payment) 
+                                    INNER JOIN usuario us ON (dp.estudiante=us.DNI) 
+                                    GROUP BY dp.estudiante";
             $result = $this->SelectAllMySQL($Query_Select_All);
             return $result;
         }
 
         public function SelectSeePayment(int $dni) {
             $this->dni = $dni;
-            $Query_Select = "SELECT pa.estudiante AS DNI, CONCAT(us.nombres, ' ', us.apellidoP, ' ', us.apellidoM) AS estudiante,
-                                    pa.periodo, COUNT(pa.estudiante) AS cantidad, pa.valor, SUM(pa.valor) AS total_pago, 
-                                    pa.estado
-                                FROM payment pa INNER JOIN usuario us ON (pa.estudiante=us.DNI) 
-                                WHERE pa.estudiante = '$this->dni' GROUP BY pa.estudiante, pa.periodo, pa.estado 
+            $Query_Select = "SELECT dp.estudiante AS DNI, CONCAT(us.nombres, ' ', us.apellidoP, ' ', us.apellidoM) AS estudiante,
+                                pa.periodo, COUNT(dp.estudiante) AS cantidad, pa.valor, SUM(pa.valor) AS total_pago, 
+                                pa.estado
+                                FROM detail_payment dp INNER JOIN payment pa ON (dp.id_detail_payment=pa.id_payment)
+                                INNER JOIN usuario us ON (dp.estudiante=us.DNI)
+                                WHERE dp.estudiante = '$this->dni' GROUP BY dp.estudiante, pa.periodo, pa.estado 
                                 ORDER BY pa.periodo DESC";
             $result = $this->SelectAllMySQL($Query_Select);
             return $result;
@@ -27,9 +30,9 @@
             $this->periodo = $periodo;
             $this->dni = $dni;
 
-            $Query_Select_All = "SELECT id_payment, tipo_pago, fecha_pago, valor, estado, observacion, descripcion 
-                                 FROM payment 
-                                 WHERE estudiante = '$this->dni' AND periodo = '$this->periodo' AND estado != 5";
+            $Query_Select_All = "SELECT pa.id_payment, pa.tipo_pago, pa.fecha_pago, pa.valor, pa.estado, pa.observacion, pa.descripcion 
+                                    FROM detail_payment dp INNER JOIN payment pa ON (dp.id_detail_payment=pa.id_payment) 
+                                    WHERE dp.estudiante = '$this->dni' AND pa.periodo = '$this->periodo' AND pa.estado != 5";
             $result = $this->SelectAllMySQL($Query_Select_All);
             return $result;
         }
@@ -38,9 +41,9 @@
             $this->periodo = $periodo;
             $this->dni = $dni;
 
-            $Query_Select_All = "SELECT id_payment, tipo_pago, fecha_pago, valor, estado, observacion, descripcion 
-                                 FROM payment 
-                                 WHERE estudiante = '$this->dni' AND periodo = '$this->periodo' AND estado = 5";
+            $Query_Select_All = "SELECT pa.id_payment, pa.tipo_pago, pa.fecha_pago, pa.valor, pa.estado, pa.observacion, pa.descripcion 
+                                    FROM detail_payment dp INNER JOIN payment pa ON (dp.id_detail_payment=pa.id_payment) 
+                                    WHERE dp.estudiante = '$this->dni' AND pa.periodo = '$this->periodo' AND pa.estado = 5";
             $result = $this->SelectAllMySQL($Query_Select_All);
             return $result;
         }
