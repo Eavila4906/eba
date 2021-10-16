6e0cf226e98f07ff673d5d400b94beadb4127658
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-16 12:28:51 -0500

    improved notifications (model)

--- Models/MyModel.php
+++ Models/MyModel.php
@@ -6,7 +6,8 @@
 
         public function SelectAllNotifications(String $user) {
             $this->user = $user;
-            $Query_Select_All = "SELECT * FROM notifications WHERE usuario = '$this->user' ORDER BY fecha DESC";
+            $Query_Select_All = "SELECT * FROM detail_notifications dn INNER JOIN notifications nt ON (dn.notifications=nt.id_notifications) 
+                                 WHERE dn.usuario = '$this->user' ORDER BY dn.date DESC";
             $result = $this->SelectAllMySQL($Query_Select_All);
             return $result;
         }
@@ -14,7 +15,8 @@
         public function SelectInfoNotificationsPayment(String $user, String $date) {
             $this->user = $user;
             $this->date = $date;
-            $Query_Select = "SELECT * FROM payment WHERE estudiante = '$this->user' AND fecha_pago = '$this->date'";
+            $Query_Select = "SELECT * FROM detail_payment dp INNER JOIN payment pa ON (dp.payment=pa.id_payment) 
+                             WHERE dp.estudiante = '$this->user' AND pa.fecha_pago = '$this->date'";
             $result = $this->SelectMySQL($Query_Select);
             return $result;
         }
@@ -29,7 +31,8 @@
 
         public function SelectNotification(String $user) {
             $this->user = $user;
-            $Query_Select = "SELECT * FROM payment WHERE estudiante = '$this->user' ORDER BY id_payment DESC LIMIT 1";
+            $Query_Select = "SELECT * FROM detail_payment dp INNER JOIN payment pa ON (dp.payment=pa.id_payment) 
+                             WHERE dp.estudiante = '$this->user' ORDER BY pa.id_payment DESC LIMIT 1";
             $result = $this->SelectMySQL($Query_Select);
             return $result;
         }
@@ -38,8 +41,10 @@
             $this->user = $user;
             $this->periodo = $periodo;
             $this->fecha = $fecha;
-            $Query_Select = "SELECT count(id_payment) AS valor FROM payment 
-                             WHERE estudiante = '$this->user' AND periodo = '$this->periodo' AND fecha_pago <= '$this->fecha'";
+            $Query_Select = "SELECT count(pa.id_payment) AS valor 
+                             FROM detail_payment dp INNER JOIN payment pa ON (dp.payment=pa.id_payment)
+                             WHERE dp.estudiante = '$this->user' 
+                             AND pa.periodo = '$this->periodo' AND pa.fecha_pago <= '$this->fecha'";
             $result = $this->SelectMySQL($Query_Select);
             $valor = $result['valor'];
             return $valor;
@@ -49,10 +54,12 @@
             $this->id_notification = $id_notification;
             $this->user = $user;
             $this->leida = 1;
-            $Query_Update = "UPDATE notifications SET leida=? WHERE id_notifications = $this->id_notification AND usuario = '$this->user'";
+            $Query_Update = "UPDATE notifications nt INNER JOIN detail_notifications dn ON (nt.id_notifications=dn.notifications) 
+                             SET nt.leida=? 
+                             WHERE nt.id_notifications = $this->id_notification AND dn.usuario = '$this->user'";
             $Array_Query = array($this->leida);
             $result = $this->UpdateMySQL($Query_Update, $Array_Query);
             return $result;
         }
-    }
+    } 
 ?>
