7093fe5e5c111f8d7c4df5f221df2291b97a05b6
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-22 14:09:18 -0500

    Nueva modal para mostrar detalles de una contabilidad inactiva.

---
+++ Views/Templates/Modals/seeIIA_modal.php
@@ -0,0 +1,71 @@
+<div class="modal fade " id="ModalSeeIIA" tabindex="-1" role="dialog" 
+  aria-labelledby="myExtraLargeModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title"><span id="titleName"></span></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="FctBtnCMSP()">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        
+        <div class="row">
+          <div class="col-md-12">
+            <div class="tile">
+              <h3 class="text-primary"></h3>
+              <div class="tile-body">
+                <div class="table-responsive">
+                  <table class="table table-hover table-bordered" id="DataTableSeeIIA" class="display" cellspacing="0" cellpadding="3" width="100%">
+                    <thead>
+                      <tr>
+                        <th>Periodo</th>
+                        <th>Ultimo pago</th>
+                        <th>Proximo pago</th>
+                        <th>Acciones</th>
+                      </tr>
+                    </thead>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row notBlock" id="dtsip">
+          <div class="col-md-12">
+            <div class="tile">
+              <h3 class="text-primary">Periodo: <span id="per"></span></h3>
+              <div class="tile-body">
+                <div class="table-responsive">
+                  <table class="table table-hover table-bordered" id="DataTableSeeIndividualPayments" class="display" cellspacing="0" cellpadding="3" width="100%">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Tipo de pago</th>
+                        <th>Fecha</th>
+                        <th>Valor</th>
+                        <th>Estado</th>
+                        <th>Observación</th>
+                        <th>Descripción</th>
+                      </tr>
+                    </thead>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <a class="btn btn-success" href="#" data-dismiss="modal" onclick="FctBtnCMSP()">
+            <i class="fa fa-fw fa-lg fas fa-arrow-alt-circle-left"></i>
+            Listo
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
