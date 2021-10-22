9d1bf336ddb7a4323974ee5b1cce35c724a9fa45
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-22 14:08:12 -0500

    Nueva modal para mostrar detalles de una contabilidad inactiva.

---
+++ Views/Templates/Modals/seeDIIA_modal.php
@@ -0,0 +1,67 @@
+<!-- Modal -->
+<div class="modal fade" id="modalSeeDIIA" tabindex="-1" role="dialog" 
+    aria-hidden="true" data-keyboard="false" data-backdrop="static">
+  <div class="modal-dialog" >
+    <div class="modal-content">
+      <div class="modal-header bg-info">
+        <h5 class="modal-title" id="titleModal-sda-diia">Periodo: <span id="periodo-diia"></span></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table table-bordered">
+            <tbody>
+                <tr>
+                    <td style="width: 200px"><b>Periodo</b></td>
+                    <td id="periodo-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Fecha ultimo pago</b></td>
+                    <td id="fup-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Fecha proximo pago</b></td>
+                    <td id="fpp-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Cuota</b></td>
+                    <td id="cuota-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Mensualidad</b></td>
+                    <td id="mensualidad-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Valor total a pagar</b></td>
+                    <td id="vtp-sda-diia"></td>
+                </tr>
+                <tr id="dp-tr-sda-diia">
+                    <td style="width: 200px"><b>Descuento</b></td>
+                    <td id="dp-sda-diia"></td>
+                </tr>
+                <tr id="vd-tr-sda-diia">
+                    <td style="width: 200px"><b>Valor de descuento</b></td>
+                    <td id="vd-sda-diia"></td>
+                </tr>
+                <tr id="vtd-tr-sda-diia">
+                    <td style="width: 200px"><b>Total con descuento</b></td>
+                    <td id="vtd-sda-diia"></td>
+                </tr>
+                <tr id="md-tr-sda-diia">
+                    <td style="width: 200px"><b>Mensualidad con descuento</b></td>
+                    <td id="md-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Descripción</b></td>
+                    <td id="descripcion-sda-diia"></td>
+                </tr>
+                <tr>
+                    <td style="width: 200px"><b>Estado</b></td>
+                    <td id="estado-sda-diia"></td>
+                </tr>
+            </table>
+      </div>
+    </div>
+  </div>
+</div>
