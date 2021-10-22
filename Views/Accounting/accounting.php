b053fc8849df039beb181bba3a0f390dbcec4974
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-22 14:14:10 -0500

    improvements in the accounting module, (view)

--- Views/Accounting/accounting.php
+++ Views/Accounting/accounting.php
@@ -3,6 +3,8 @@
   getModal('startsAccounting_modal', $data);
   getModal('totalPurchaseAccounting_modal', $data);
   getModal('seeDetailsAccounting_modal', $data);
+  getModal('seeIIA_modal', $data);
+  getModal('seeDIIA_modal', $data);
 ?>    
 <main class="app-content">
     <div class="app-title">
@@ -25,10 +27,10 @@
                 <table class="table table-hover table-bordered" id="DataTableIC">
                   <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Cedula</th>
-                        <th>Estudiante</th>
-                        <th>Acción</th>
+                      <th>ID</th>
+                      <th>Cedula</th>
+                      <th>Estudiante</th>
+                      <th>Acción</th>
                     </tr>
                   </thead>
                 </table>
@@ -42,15 +44,37 @@
             <h3 class="text-primary">Contabilidad activa</h3>
             <div class="tile-body">
               <div class="table-responsive">
+                <table class="table table-hover table-bordered" id="DataTableCA">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Cedula</th>
+                      <th>Estudiante</th>
+                      <th>Periodo</th>
+                      <th>Ultimo pago</th>
+                      <th>Proximo pago</th>
+                      <th>Acción</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <div class="tile">
+            <h3 class="text-primary">Contabilidad inactiva</h3>
+            <div class="tile-body">
+              <div class="table-responsive">
                 <table class="table table-hover table-bordered" id="DataTableCI">
                   <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Estudiante</th>
-                        <th>Periodo</th>
-                        <th>Ultimo pago</th>
-                        <th>Proximo pago</th>
-                        <th>Acción</th>
+                      <th>ID</th>
+                      <th>Cedula</th>
+                      <th>Estudiante</th>
+                      <th>Periodos</th>
+                      <th>Acción</th>
                     </tr>
                   </thead>
                 </table>
