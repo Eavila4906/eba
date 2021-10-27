defa1a6b531cff1844ebab0869d56b1f2aaaeb39
eavila4906-utm <eavila4906@utm.edu.ec>
2021-10-27 10:23:52 -0500

    Improved logout

--- Controllers/Logout.php
+++ Controllers/Logout.php
@@ -1,15 +1,21 @@
 <?php
     class Logout {
         public function __construct() {
-            session_start();
+            if(!isset($_SESSION)) { 
+                session_start(); 
+            }
             $session = $_SESSION['dataUser']['nombreRol'];
             if ($session != "Super Administrador") {
-                session_start();
+                if(!isset($_SESSION)) { 
+                    session_start(); 
+                }
                 session_unset();
                 session_destroy();
                 header('location: '.BASE_URL());
             } else {
-                session_start();
+                if(!isset($_SESSION)) { 
+                    session_start(); 
+                }
                 session_unset();
                 session_destroy();
                 header('location: '.BASE_URL().'admin');
