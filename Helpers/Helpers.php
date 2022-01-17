2a5b46c0f3dd693e2201a3658f65407d4af649f9
eavila4906-utm <eavila4906@utm.edu.ec>
2022-01-17 11:57:12 -0500

    new text cleanup feature

--- Helpers/Helpers.php
+++ Helpers/Helpers.php
@@ -189,4 +189,26 @@
         $result = $year.'-'.$month.'-';
         return $result;
     }
+
+    //Elimina exceso de espacios entre palabras
+    function strClean2($strCadena){
+        $string = preg_replace(['/\s+/','/^\s|\s$/'],[' ',''], $strCadena);
+        $string = trim($string); //Elimina espacios en blanco al inicio y al final
+        $string = stripslashes($string); // Elimina las \ invertidas
+        $string = str_ireplace("<script>","",$string);
+        $string = str_ireplace("</script>","",$string);
+        $string = str_ireplace("<script src>","",$string);
+        $string = str_ireplace("<script type=>","",$string);
+        $string = str_ireplace("SELECT * FROM","",$string);
+        $string = str_ireplace("DELETE FROM","",$string);
+        $string = str_ireplace("INSERT INTO","",$string);
+        $string = str_ireplace("SELECT COUNT(*) FROM","",$string);
+        $string = str_ireplace("DROP TABLE","",$string);
+        $string = str_ireplace("--","",$string);
+        $string = str_ireplace("^","",$string);
+        $string = str_ireplace("[","",$string);
+        $string = str_ireplace("]","",$string);
+        $string = str_ireplace("==","",$string);
+        return $string;
+    }
 ?>
