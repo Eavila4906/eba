6cd0097c0dc4eca29a431cf282786860382afa87
Eavila4906 <eavila4906@gmail.com>
2022-09-30 12:39:35 -0500

    Creating the function to extract and show the data in the table (Middleware)

--- Assets/js/functions_courses.js
+++ Assets/js/functions_courses.js
@@ -1,4 +1,12 @@
-
+$(document).ready(function () {
+	if (document.querySelector('#module-courses')) {
+		document.querySelector('#module-courses').classList.add('is-expanded');
+		if (document.querySelector('#icon-courses')) {
+            document.querySelector('#icon-courses').classList.replace('fa-circle-o', 'fa-circle');
+            document.querySelector('#icon-courses').classList.add('text-primary');
+        }
+	}
+});
 
 function openModalCurses() {
 	document.querySelector('#id_curses').value = "";
@@ -10,3 +18,36 @@ function openModalCurses() {
 	//cleanResiduoVali();
 	$('#ModalFormCurses').modal('show');
 }
+
+var DataTableCourses;
+document.addEventListener('DOMContentLoaded', function () {
+
+	DataTableCourses = $('#DataTableCourses').DataTable({ /*ID de la tabla*/
+		"aProcessing": true,
+		"aServerside": true,
+		"language": {
+			"url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json" /*Idioma de visualizacion*/
+		},
+		"ajax": {
+			"url": BASE_URL + "courses/getAllCourses",
+			"dataSrc": ""
+		},
+		"columns": [/* Campos de la base de datos*/
+			{ "data": "id_course" },
+			{ "data": "name" },
+			{ "data": "category" },
+			{ "data": "description" },
+			{ "data": "date_start" },
+			{ "data": "date_final" },
+			{ "data": "value" },
+			{ "data": "status" },
+			{ "data": "Acciones" },
+		],
+		"responsieve": "true",
+		"bDestroy": true,
+		"iDisplayLength": 10, /*Mostrará los primero 10 registros*/
+		"order": [[0, "desc"]] /*Ordenar de forma Desendente*/
+	});
+
+    
+});
