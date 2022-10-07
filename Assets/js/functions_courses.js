330c7b2fd1e585f1f99c6bcdc2c2207aea572481
Eavila4906 <eavila4906@gmail.com>
2022-10-06 20:46:16 -0500

    Error correction (Middleware)

--- Assets/js/functions_courses.js
+++ Assets/js/functions_courses.js
@@ -8,45 +8,6 @@ $(document).ready(function () {
 	}
 });
 
-function openModalCourse() {
-	document.querySelector('#id_course').value = "";
-	document.querySelector('#title-modal-course').innerHTML = "Nuevo Curso";
-	document.querySelector('#modal-header-course').classList.replace("header-update", "header-register");
-	document.querySelector('#btn-action-form').classList.replace("btn-info", "btn-success");
-	document.querySelector('#text-btn').innerHTML = "Guardar";
-	document.querySelector('#formCourse').reset();
-	//cleanResiduoVali();
-	$('#ModalFormCourse').modal('show');
-}
-
-//show list category
-$(document).ready(function () {
-	var categoryList = document.querySelector('.category-register');
-	var se = `<option class="none-block" value="${objData.data.id_course_category}">${objData.data.category}</option>`;
-	function loadCategoryList() {
-		$.ajax({
-			type: 'POST',
-			url: 'category/getCategoryList',
-			success: function (data) {
-				var objData = JSON.parse(data);
-
-				var selectedDisable = se;
-
-				objData.forEach(objData => {
-					selectedDisable += `
-						<option value="${objData.id_course_category}">${objData.category}</option>
-					`;
-				});
-
-				categoryList.innerHTML = selectedDisable;
-			}
-		});
-	}
-
-	loadCategoryList();
-});
-//
-
 var DataTableCourses;
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -69,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			{ "data": "date_final" },
 			{ "data": "value" },
 			{ "data": "status" },
-			{ "data": "Acciones" },
+			{ "data": "Acciones" }
 		],
 		"responsieve": "true",
 		"bDestroy": true,
@@ -127,6 +88,38 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 });
 
+function openModalCourse() {
+	document.querySelector('#id_course').value = "";
+	document.querySelector('#title-modal-course').innerHTML = "Nuevo Curso";
+	document.querySelector('#modal-header-course').classList.replace("header-update", "header-register");
+	document.querySelector('#btn-action-form').classList.replace("btn-info", "btn-success");
+	document.querySelector('#text-btn').innerHTML = "Guardar";
+	document.querySelector('#formCourse').reset();
+	//show list category
+	$(document).ready(function () {
+		var categoryList = document.querySelector('.category-register');
+		function loadCategoryList() {
+			$.ajax({
+				type: 'GET',
+				url: 'courses/getCategoryList',
+				success: function (data) {
+					var objData = JSON.parse(data);
+					var selectedDisable = '<option class="form-control none-block" selected disabled> Seleccionar categoría</option>';
+					objData.forEach(objData => {
+						selectedDisable += `
+							<option value="${objData.id_course_category}">${objData.category}</option>`;
+					});
+					categoryList.innerHTML = selectedDisable;
+				}
+			});
+		}
+		loadCategoryList();
+	});
+	//
+	//cleanResiduoVali();
+	$('#ModalFormCourse').modal('show');
+}
+
 function FctBtnUpdateCourse(id_course) {
 	document.querySelector('#title-modal-course').innerHTML = "Actualizar categoría";
 	document.querySelector('#modal-header-course').classList.replace("header-register","header-update");
@@ -137,9 +130,9 @@ function FctBtnUpdateCourse(id_course) {
 	campos.InputCourse = true; 
 	campos.InputDescription = true;*/
 
-	var id_category = id_category;
+	var id_course = id_course;
 	var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-	var ajaxUrl = BASE_URL + 'courses/getCourse/' + id_category;
+	var ajaxUrl = BASE_URL + 'courses/getCourse/' + id_course;
 	request.open("GET", ajaxUrl, true);
 	request.send();
 	
@@ -149,14 +142,17 @@ function FctBtnUpdateCourse(id_course) {
 			if (objData.status) {
 				document.querySelector('#id_course').value = objData.data.id_course;
 				document.querySelector('#InputCourse').value = objData.data.name;
+				document.querySelector('#InputDateStart').value = objData.data.date_start;
+				document.querySelector('#InputDateFinal').value = objData.data.date_final;
+				document.querySelector('#InputValueCourse').value = objData.data.value;
 				//show list category
 				$(document).ready(function () {
 					var categoryList = document.querySelector('.category-update');
-					var se = `<option class="none-block" value="${objData.data.id_course_category}">${objData.data.category}</option>`;
+					var se = `<option class="none-block" value="${ objData.data.id_course_category}">${objData.data.category}</option>`;
 					function loadCategoryList() {
 						$.ajax({
-							type: 'POST',
-							url: 'category/getCategoryList',
+							type: 'GET',
+							url: 'courses/getCategoryList',
 							success: function (data) {
 								var objData = JSON.parse(data);
 
@@ -190,7 +186,7 @@ function FctBtnUpdateCourse(id_course) {
 									<option value="2">Inactivo</option>
 								`;
 
-				document.querySelector('#ListaEstadoRol').innerHTML = SelectHTML;
+				document.querySelector('#InputStatus').innerHTML = SelectHTML;
 
 			} else {
 				swal("ERROR!", objData.msg, "error");
