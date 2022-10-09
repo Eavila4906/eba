6e6c70831785c1b9c722ff9cc822c56a05bfdc2f
Steven Urdanigo <surdanigo9914@utm.edu.ec>
2022-10-08 20:09:04 -0500

    modals info_course

---
+++ Views/Templates/Modals/info_course_modal.php
@@ -0,0 +1,71 @@
+<div class="modal fade" id="ModalInfoCourse" tabindex="-1" role="dialog" 
+  aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header header-infoCourse">
+        <h5 class="modal-title" id="title-modal"><i class="fa fa-user col-1"></i> <b id="title"></b></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+
+      <!-- info course -->
+      <div class="tab-pane active" >
+      <input type="hidden" id="id_Course" name="id_Course" value="">
+              <div class="timeline-post">
+                <div class="post-media"> 
+                  <div class="content">
+                    <h5 class="text-primary">Info del curso</h5> 
+                  </div>
+                </div>
+
+                <table class="table table-bordered">
+                  <tbody>
+                    <tr>
+                      <td style="width: 150px">Curso</td>
+                      <td id="getCourse"></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 150px">Categoria</td>
+                      <td id="getCategory"></td>
+                    </tr>
+
+                    <tr>
+                      <td style="width: 150px">Fecha de inicio</td>
+                      <td id="getDateStart"></td>
+                    </tr>
+
+                    <tr>
+                      <td style="width: 150px">Fecha final</td>
+                      <td id="getDateFinal"></td>
+                    </tr>
+
+                    <tr>
+                      <td style="width: 150px">Valor del curso</td>
+                      <td id="getValueCourse"></td>
+                    </tr>
+
+                    <tr>
+                      <td style="width: 150px">Descripción</td>
+                      <td id="getDescription"></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 150px">Estado</td>
+                      <td id="getStatus"></td>
+                    </tr>
+
+                  </tbody>
+                </table>
+
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
