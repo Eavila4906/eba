59ae05df31409b354933388059507a81028e5aa8
Eavila4906 <eavila4906@gmail.com>
2022-10-08 23:26:35 -0500

    Changes in the design of the course info modal

--- Views/Templates/Modals/info_course_modal.php
+++ Views/Templates/Modals/info_course_modal.php
@@ -1,71 +1,51 @@
 <div class="modal fade" id="ModalInfoCourse" tabindex="-1" role="dialog" 
   aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header header-infoCourse">
-        <h5 class="modal-title" id="title-modal"></i> <b id="title"></b></h5>
+        <h5 class="modal-title" id="title-modal"></i>Curso / <spam id="getTitleCourese"></spam></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
-
-      <!-- info course -->
-      <div class="tab-pane active" >
-      <input type="hidden" id="id_Course" name="id_Course" value="">
-      <!-- <input type="hidden" id="getIdCourse" name="getIdCourse" value=""> -->
-              <div class="timeline-post">
-                <div class="post-media"> 
-                  <div class="content">
-                    <h5 class="text-primary">Info del curso</h5> 
-                  </div>
-                </div>
-
-                <table class="table table-bordered">
-                  <tbody>
-                    <tr>
-                      <td style="width: 150px">Curso</td>
-                      <td id="getCourse"></td>
-                    </tr>
-                    <tr>
-                      <td style="width: 150px">Categoria</td>
-                      <td id="getCategory"></td>
-                    </tr>
-
-                    <tr>
-                      <td style="width: 150px">Fecha de inicio</td>
-                      <td id="getDateStart"></td>
-                    </tr>
-
-                    <tr>
-                      <td style="width: 150px">Fecha final</td>
-                      <td id="getDateFinal"></td>
-                    </tr>
-
-                    <tr>
-                      <td style="width: 150px">Valor del curso</td>
-                      <td id="getValueCourse"></td>
-                    </tr>
-
-                    <tr>
-                      <td style="width: 150px">Descripción</td>
-                      <td id="getDescription"></td>
-                    </tr>
-                    <tr>
-                      <td style="width: 150px">Estado</td>
-                      <td id="getStatus"></td>
-                    </tr>
-
-                  </tbody>
-                </table>
-
-
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="modal-body">
+        <table class="table table-bordered">
+          <tbody>
+            <tr>
+              <td style="width: 200px"><b>ID</b></td>
+              <td id="getIdCourse"></td>
+            </tr>
+            <tr>
+              <td style="width: 200px"><b>Curso</b></td>
+              <td id="getCourse"></td>
+            </tr>
+            <tr>
+              <td style="width: 200px"><b>Categoría</b></td>
+              <td id="getCategory"></td>
+            </tr>
+            <tr>
+              <td style="width: 200px"><b>Fecha de inicio</b></td>
+              <td id="getDateStart"></td>
+            </tr>
+            <tr>
+              <td style="width: 150px"><b>Fecha final</b></td>
+              <td id="getDateFinal"></td>
+            </tr>
+            <tr>
+              <td style="width: 150px"><b>Valor del curso</b></td>
+              <td id="getValueCourse"></td>
+            </tr>
+            <tr>
+              <td style="width: 150px"><b>Descripción</b></td>
+              <td id="getDescription"></td>
+            </tr>
+            <tr>
+              <td style="width: 150px"><b>Estado</b></td>
+              <td id="getStatus"></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
