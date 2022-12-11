ad3f0073047a9e81642801a36e63f06c12b874ce
Eavila4906 <eavila4906@gmail.com>
2022-12-11 11:47:44 -0500

    Optimizing existing classes and functions

--- ASP/asp.php
+++ ASP/asp.php
@@ -1,79 +1,7 @@
 <?php
-    //url project
-    const BASE_URL = "http://localhost/eba/";
-    //data of connection database
-    const DB_HOST = "localhost";
-    const DB_USER = "root";
-    const DB_PASSWORD = "";
-    const DB_NAME = "eba_db_p";
-    const DB_CHARSET = "utf8";
-    
-    //data of seend email
-    const SENDER_NAME = "English Bootcamp Academy";
-	const SENDER_EMAIL = "eavila4906@gmail.com";
-	const COMPANY_NAME = "English Bootcamp Academy";
-	const WEB_COMPANY = "www.eba.com.ec";
-
-    class ConecctionDB {
-        private $connec;
-        public function __construct(){
-            $str_Conecction = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET;
-            try {
-                $this->connec =new PDO($str_Conecction, DB_USER, DB_PASSWORD);
-                $this->connec->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                $this->connec = "FILED CONNECTION!";
-                echo "ERROR: ".$e->getMessage();
-            }
-        }
-        public function Connec(){
-            return $this->connec;
-        }
-    }
-
-    class MySQL extends ConecctionDB {
-        private $conexion;
-        private $strQuery;
-        private $arrayValues;
-
-        public function __construct() {
-            $this->conexion =new ConecctionDB();
-            $this->conexion = $this->conexion->Connec();
-        }
-
-        public function SelectAllMySQL(String $Query) {
-            $this->strQuery = $Query;
-            $result = $this->conexion->prepare($this->strQuery);
-            $result->execute();
-            $data = $result->fetchall(PDO::FETCH_ASSOC);
-            return $data;
-        }
-
-        public function InsertMySQL(String $Query, array $arrayValues) {
-            $this->strQuery = $Query;
-            $this->arrayValues = $arrayValues;
-            $Insert = $this->conexion->prepare($this->strQuery);
-            $resul_insert = $Insert->execute($this->arrayValues);
-            if ($resul_insert) {
-                $lastinsert = $this->conexion->lastinsertId();
-            } else {
-                $lastinsert = 0;
-            }
-            return $lastinsert;
-        }
-
-        public function DeleteMySQL(String $Query) {
-            $this->Query = $Query;
-            $result = $this->conexion->prepare($this->Query);
-            $result->execute();
-            if ($result) {
-                $lastEliminar = 1;
-            } else {
-                $lastEliminar = 0;
-            }
-            return $lastEliminar;
-        }
-    }
+    require_once("../Config/Config.php");
+    require_once("../Libraries/Core/ConecctionDB.php");
+    require_once("../Libraries/Core/MySQL.php");
 
     class Model extends MySQL {
         public function __construct(){
@@ -231,5 +159,4 @@
 
     $ASP =new AutomaticSystemProcesses();
     $ASP->run();
-
 ?>
